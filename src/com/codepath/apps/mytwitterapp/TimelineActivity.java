41cadd7e512514a05c05d0e046a419c8976a145d
Alex Tam <alextam07@gmail.com>
2013-09-07 00:19:03 -0700

    Initial commit

---
+++ src/com/codepath/apps/mytwitterapp/TimelineActivity.java
@@ -0,0 +1,44 @@
+package com.codepath.apps.mytwitterapp;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ListView;
+
+import com.codepath.apps.mytwitterapp.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+public class TimelineActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timeline);
+		
+		MyTwitterApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONArray jsonTweets) {
+				ArrayList<Tweet> tweets = Tweet.fromJson(jsonTweets);
+				ListView lvTweets = (ListView)findViewById(R.id.lvTweets);
+				TweetsAdapter adapter = new TweetsAdapter(getBaseContext(), tweets);
+				lvTweets.setAdapter(adapter);
+				
+				
+				// Log.d("DEBUG", jsonTweets.toString());
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.timeline, menu);
+		return true;
+	}
+
+}
